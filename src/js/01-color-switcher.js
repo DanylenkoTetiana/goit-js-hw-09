4086bbf05b484a3e056089eea37d2168099f56bd
DanylenkoTetiana <danilenko.tanya@i.ua>
2022-10-10 13:42:25 +0300

    додає рішення завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+'use strict';
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+let statusTimerId = false;
 
+startBtn.addEventListener('click', () => {
+  if (statusTimerId) {
+    return;
+  }
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    statusTimerId = true;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  statusTimerId = false;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
