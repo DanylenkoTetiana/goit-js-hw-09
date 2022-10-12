cd922fe6ac1c5ac6714f226b5375ed0bc9c20cb7
DanylenkoTetiana <danilenko.tanya@i.ua>
2022-10-12 16:02:54 +0300

    додає рішення завдання 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,32 @@
 'use strict';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const timerEl = document.querySelector('.timer');
+timerEl.style.display = 'flex';
+timerEl.style.gap = '15px';
+timerEl.style.textAlign = 'center';
+timerEl.style.paddingTop = '20px';
+
+const valueEls = document.querySelectorAll('.value');
+valueEls.forEach(valueEl => {
+  valueEl.style.display = 'block';
+  valueEl.style.fontWeight = '700';
+  valueEl.style.fontSize = '28px';
+});
+const labelEls = document.querySelectorAll('.label');
+labelEls.forEach(labelEl => {
+  labelEl.style.fontSize = '12px';
+  labelEl.style.textTransform = 'uppercase';
+  labelEl.style.fontWeight = '600';
+});
+
 const inputEl = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
+startBtn.disabled = true;
 let timerId = null;
+let selectTime = 0;
 
 const options = {
   enableTime: true,
@@ -11,22 +34,48 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] < new Date()) {
-      alert('Please choose a date in the future');
-      console.log(selectedDates[0]);
+    if (selectTime > 0) {
+      Notify.failure(`Please reload the page`);
+      return;
+    } else if (selectedDates[0] < new Date()) {
+      Notify.failure('Please choose a date in the future');
     } else if (selectedDates[0] > new Date()) {
-      startBtn.addEventListener('click', () => {
-        timerId = setInterval(() => {
-          convertMs(5000);
-          console.log('helloy');
-          // document.body.style.backgroundColor = getRandomHexColor();
-        }, 1000);
-      });
+      startBtn.disabled = false;
+      selectTime = selectedDates[0].getTime();
     }
   },
 };
 flatpickr(inputEl, options);
-console.log(convertMs(5000000000));
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  timerId = setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = selectTime - currentTime;
+    if (deltaTime <= 0) {
+      clearInterval(timerId);
+      return;
+    }
+    const times = convertMs(deltaTime);
+    document.querySelector('span[data-seconds]').textContent = addLeadingZero(
+      times.seconds
+    );
+
+    document.querySelector('span[data-minutes]').textContent = addLeadingZero(
+      times.minutes
+    );
+    document.querySelector('span[data-hours]').textContent = addLeadingZero(
+      times.hours
+    );
+    document.querySelector('span[data-days]').textContent = addLeadingZero(
+      times.days
+    );
+  }, 1000);
+});
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
