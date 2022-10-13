907108ea3295cfcac10b9e23ce68f8db64db9b09
DanylenkoTetiana <danilenko.tanya@i.ua>
2022-10-13 20:39:44 +0300

    усуває помилку в 3-й задачі

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,36 +4,24 @@ const formEl = document.querySelector('.form');
 const amountEl = document.querySelector('input[name=amount]');
 const delayEl = document.querySelector('input[name=delay]');
 const stepEl = document.querySelector('input[name= step]');
-// console.log(formEl);
-// const args = [];
-// const numbersArrey = [];
-// const delayArrey = [];
 
 formEl.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   let calculatedDelay = Number(delayEl.value) - Number(stepEl.value);
-  // let numbers = 1;
   for (let i = 1; i <= amountEl.value; i += 1) {
-    // numbersArrey.push(i);
-
     calculatedDelay += Number(stepEl.value);
-    // delayArrey.push(calculatedDelay);
-
     createPromise(i, calculatedDelay)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-    console.log(createPromise(i, calculatedDelay));
-    // console.log(numbersArrey);
-    // console.log(delayArrey);
   }
 }
-
-// console.log(createPromise(1, 3000));
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
@@ -48,3 +36,41 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
+
+// formEl.addEventListener('submit', handleSubmit);
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   let calculatedDelay = Number(delayEl.value) - Number(stepEl.value);
+//   for (let i = 1; i <= amountEl.value; i += 1) {
+//     calculatedDelay += Number(stepEl.value);
+//     args.push({ i, calculatedDelay });
+//   }
+
+//   const promises = args.map(arg => {
+//     const { i, calculatedDelay } = arg;
+//     console.log(createPromise(i, calculatedDelay));
+//     return createPromise(i, calculatedDelay)
+//       .then(({ position, delay }) => {
+//         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//         // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       })
+//       .catch(({ position, delay }) => {
+//         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//         // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//       });
+//   });
+// }
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
